74418586e142f73b2bdb28fccdb1975118707d62
BasselAlhomsi <basselalhomsi9@gmail.com>
2024-09-27 21:47:23 +0300

    edit on database

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Http\Request;
+
+use App\Http\Controllers\api\company\CompanyLoginController;
+use App\Http\Controllers\api\company\CompanyRegisterController;
+use App\Http\Controllers\api\researcher\ResearcherLoginController;
+use App\Http\Controllers\api\researcher\ResearcherRegisterController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +20,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+Route::post('/researcherregister', [ResearcherRegisterController::class, 'store']);
+Route::post('/researcherregister/{uuid}', [ResearcherRegisterController::class,'registerCode']);
+Route::post('/researcherlogin',[ResearcherLoginController::class,'login']);
+Route::post('/researcherlogout',[ResearcherLoginController::class,'logout'])->middleware('auth:sanctum');
+
+Route::post('/companyregister', [CompanyRegisterController::class, 'store']);
+Route::post('/companylogin',[CompanyLoginController::class,'login']);
+Route::post('/companylogout',[CompanyLoginController::class,'logout'])->middleware('auth:sanctum');
