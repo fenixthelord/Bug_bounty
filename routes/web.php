dfe36f32089125b512e09617a893c28f1c43c887
hazem tayara <hazemtayara36@gmail.com>
2024-09-29 13:36:48 +0300

    some updates

--- routes/web.php
+++ routes/web.php
@@ -37,21 +37,21 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 //Auth::routes();
 
-Route::get('/test', [DashboardController::class, 'index'])->name('Admin-Panel');
+// Route::get('/test', action: [DashboardController::class, 'index'])->name('Admin-Panel');
 Route::get('/404', [DashboardController::class, 'notFound'])->name('404');
 //Route::get('/500', [DashboardController::class, 'serverError'])->name('404');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [DashboardController::class, 'index'])->name('home');
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home/specializations', [SpecializationController::class, 'index'])->name('specializations');
-Route::get('/home/specializations/create', [SpecializationController::class, 'create'])->name('specializations.create');
-Route::post('/home/specializations/store', [SpecializationController::class, 'store'])->name('specializations.store');
+Route::get('/home/specializations/create', [SpecializationController::class, 'create'])->name('specializations.createe');
+Route::post('/home/specializations/store', [SpecializationController::class, 'store'])->name('specializations.storee');
 Route::get('/home/specializations/{id}/companies', [SpecializationController::class, 'show'])->name('specialization.companies');
-Route::get('/home/specializations/{specialization}/edit', [SpecializationController::class, 'edit'])->name('specializations.edit');
-Route::put('/home/specializations/{specialization}', [SpecializationController::class, 'update'])->name('specializations.update');
+Route::get('/home/specializations/{specialization}/edit', [SpecializationController::class, 'edit'])->name('specializations.edite');
+Route::put('/home/specializations/{specialization}', [SpecializationController::class, 'update'])->name('specializations.updatee');
 Route::resource('specializations', SpecializationController::class);
 Route::post('/specializations/restore/{id}', [SpecializationController::class, 'restore'])->name('specializations.restore');
 Route::get('/trashed', function () {
